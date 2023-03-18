d59cf8b99aac3b9c70c4dd2f3a99056a9c3759a3
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-18 18:58:27 +0300

    add gameover banner

--- animations/animate_spaceship.py
+++ animations/animate_spaceship.py
@@ -4,7 +4,17 @@ from itertools import cycle
 from curses_tools import read_controls, get_frame_size
 from physics import update_speed
 from animations.fire_animation import fire
+from animations.stars import sleep
 
+
+async def game_over(canvas, center_row, center_column, gameover_frame):
+    gameover_size_rows, gameover_size_columns = get_frame_size(gameover_frame)
+    center_column -= gameover_size_columns*0.5
+    center_row-= gameover_size_rows*0.5
+    while True:
+        draw_frame(canvas, center_row, center_column, gameover_frame, negative=False)
+        await asyncio.sleep(0)
+                    
 
 async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2, coroutines, obstacles, obstacles_in_last_collision):
    
@@ -14,7 +24,19 @@ async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2,
     
     row_speed = column_speed = 0
 
+    center_row = row
+    center_column = column 
+
+    with open("gameover.txt", "r") as gameover_file:    
+            gameover_frame = gameover_file.read()
+
+
+    game_over_async = game_over(canvas, center_row, center_column, gameover_frame)
+
     for frame in cycle(frames):
+
+        
+
 
         number_of_rows, number_of_columns = canvas.getmaxyx()
         ship_size_rows, ship_size_columns = get_frame_size(frame)
@@ -44,5 +66,13 @@ async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2,
         column += column_speed
 
 
+        for obstacle in obstacles:
+            if obstacle.has_collision(row, column, ship_size_rows, ship_size_columns):
+                coroutines.append(game_over_async)
+                return
+
         if is_shot_pressed:
             coroutines.append(fire(canvas, row, column, obstacles, obstacles_in_last_collision))
+
+
+
