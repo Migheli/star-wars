c1c55e425578526aa0dabac0177a4e9acea01b53
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-21 20:31:49 +0300

    pep8 animate_spaceship

--- animations/animate_spaceship.py
+++ animations/animate_spaceship.py
@@ -4,33 +4,29 @@ from itertools import cycle
 from curses_tools import read_controls, get_frame_size
 from physics import update_speed
 from animations.fire_animation import fire
-from animations.stars import sleep
-import time 
+import time
 import math
 from animations.game_messages import show_game_over
 
+
 async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2, coroutines, obstacles, obstacles_in_last_collision, start_time, start_year, gun_available_year):
-   
     frames = [rocket_frame_1, rocket_frame_2]
-    
     input_controls = []
-    
     row_speed = column_speed = 0
 
     center_row = row
-    center_column = column 
+    center_column = column
 
-    with open("frames/gameover.txt", "r") as gameover_file:    
-            gameover_frame = gameover_file.read()
-
+    with open("frames/gameover.txt", "r") as gameover_file:
+        gameover_frame = gameover_file.read()
 
     game_over_view = show_game_over(canvas, center_row, center_column, gameover_frame)
 
     for frame in cycle(frames):
-        
+
         number_of_rows, number_of_columns = canvas.getmaxyx()
         ship_size_rows, ship_size_columns = get_frame_size(frame)
-        
+
         ship_border_row = number_of_rows - ship_size_rows
         ship_border_column = number_of_columns - ship_size_columns
         if row > ship_border_row:
@@ -41,14 +37,13 @@ async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2,
             row = 0
         if column < 0:
             column = 0
-        
+
         draw_frame(canvas, row, column, frame)
         await asyncio.sleep(0)
-        
+
         draw_frame(canvas, row, column, frame, negative=True)
         input_controls = read_controls(canvas)
         row_direction, column_direction, is_shot_pressed = input_controls
-        
         row_speed, column_speed = update_speed(row_speed, column_speed, row_direction, column_direction)
 
         row += row_speed
@@ -59,9 +54,9 @@ async def animate_spaceship(canvas, row, column, rocket_frame_1, rocket_frame_2,
                 coroutines.append(game_over_view)
                 return
 
-        seconds_left = time.time() - start_time 
+        seconds_left = time.time() - start_time
         years_left = math.floor(seconds_left/1.5)
         year = years_left + start_year
-        
+
         if year > gun_available_year and is_shot_pressed:
             coroutines.append(fire(canvas, row, column, obstacles, obstacles_in_last_collision))
