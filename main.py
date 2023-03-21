d5fff7b71a81e5bde2c8dcf8a3ccc838aeb2f130
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-21 20:26:35 +0300

    pep8 main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-import time
-import curses
-from random import randint, choice
-from animations.fire_animation import fire
-from animations.animate_spaceship import animate_spaceship
-from animations.stars import blink
-from animations.space_garbage import fly_garbage
-from animations.stars import sleep
-from animations.obstacles import show_obstacles
-import time 
-import math
-from game_scenario import get_garbage_delay_tics, PHRASES
-
 import asyncio
+import curses
+import math
+import time
+from random import randint, choice
+from animations.animate_spaceship import animate_spaceship
+from animations.stars import blink, sleep
+from animations.space_garbage import fly_garbage
+from game_scenario import get_garbage_delay_tics, PHRASES
 
 
 TIC_TIMEOUT = 0.1
@@ -24,17 +19,17 @@ obstacles_in_last_collisions = []
 
 
 async def show_current_year(canvas):
-        while True:
-            canvas.addstr(1, 1, str(year))
-            canvas.refresh()
-            await asyncio.sleep(0)
+    while True:
+        canvas.addstr(1, 1, str(year))
+        canvas.refresh()
+        await asyncio.sleep(0)
 
 
 async def show_phrases(canvas):
     while True:
         if year in PHRASES:
             canvas.addstr(1, 6, PHRASES[year])
-        else: 
+        else:
             canvas.addstr(1, 6, ' ' * 45)
         canvas.refresh()
         await asyncio.sleep(0)
@@ -42,22 +37,21 @@ async def show_phrases(canvas):
 
 async def update_current_year(start_time):
     while True:
-        seconds_left = time.time() - start_time 
+        seconds_left = time.time() - start_time
         years_left = math.floor(seconds_left/1.5)
-        global year 
+        global year
         year = START_YEAR + years_left
         await asyncio.sleep(0)
 
 
 async def fill_orbit_with_garbage(canvas, garbage, start_column, border_x, obstacles, obstacles_in_last_collisions):
-        while True:
-            if year:
-                garbage_delay = get_garbage_delay_tics(year)
-                if garbage_delay:
-                        coroutines.append(fly_garbage(canvas, randint(start_column, border_x), choice(garbage), obstacles, obstacles_in_last_collisions))
-                        await sleep(garbage_delay)
-            await asyncio.sleep(0)
-
+    while True:
+        if year:
+            garbage_delay = get_garbage_delay_tics(year)
+            if garbage_delay:
+                coroutines.append(fly_garbage(canvas, randint(start_column, border_x), choice(garbage), obstacles, obstacles_in_last_collisions))
+                await sleep(garbage_delay)
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
@@ -71,8 +65,6 @@ def draw(canvas):
     window_width, window_height = curses.window.getmaxyx(canvas)
     border_y, border_x = window_width-1, window_height-1
 
-    """ window.getmaxyx - return tuple of values width and height of window, not coordinates of last cells in rows and columns. Numeration of width and height of window starts from 0 and it values always more, than coordinates of last cells in rows/columns (+1). That's why we need to makes (-1) in border_x, border_y declaration """
-
     with open("frames/trash_small.txt", "r") as trash_small, open("frames/trash_large.txt", "r") as trash_large, open("frames/trash_xl.txt", "r") as trash_xl:
         garbage = [trash_small.read(), trash_large.read(), trash_xl.read()]
 
@@ -80,16 +72,12 @@ def draw(canvas):
 
     stars_signs = ['+', '*', '.', ':']
     start_row = start_column = 1
-    
-
     coroutines.append(update_current_year(start_time))
-    stat_canvas = canvas.derwin(1, 1) 
+    stat_canvas = canvas.derwin(1, 1)
     coroutines.append(show_current_year(stat_canvas))
     coroutines.append(show_phrases(stat_canvas))
-    
     ship = animate_spaceship(canvas, center_y, center_x, rocket_frame_1, rocket_frame_2, coroutines, obstacles, obstacles_in_last_collisions, start_time, START_YEAR, GUN_AVAILABLE_YEAR)
     coroutines.append(ship)
-    
     min_number_of_stars = 10
     max_number_of_stars = 145
 
@@ -97,10 +85,7 @@ def draw(canvas):
         offset_tics = randint(3, 20)
         coroutines.append(blink(canvas, randint(start_row, border_y), randint(start_column, border_x), choice(stars_signs), offset_tics))
 
-
     coroutines.append(fill_orbit_with_garbage(canvas, garbage, start_column, border_x, obstacles, obstacles_in_last_collisions))
-    #coroutines.append(show_obstacles(canvas, obstacles))
-
 
     while True:
         for coroutine in coroutines.copy():
@@ -108,7 +93,6 @@ def draw(canvas):
                 coroutine.send(None)
             except StopIteration:
                 coroutines.remove(coroutine)
-        
         if not len(coroutines):
             break
 
