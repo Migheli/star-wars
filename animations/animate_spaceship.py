197dfe2689e2359a4efd8289e1e5fa2af4518f09
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-25 11:02:34 +0300

    set spaceship animation

--- animations/animate_spaceship.py
+++ animations/animate_spaceship.py
@@ -23,37 +23,41 @@ async def animate_spaceship(canvas, row, column, rocket_animation, coroutines, o
 
     for frame in cycle(rocket_animation):
 
-        number_of_rows, number_of_columns = canvas.getmaxyx()
-        ship_size_rows, ship_size_columns = get_frame_size(frame)
+        for step in range(2):
 
-        ship_border_row = number_of_rows - ship_size_rows
-        ship_border_column = number_of_columns - ship_size_columns
-          
-        row = min(row, ship_border_row)
-        row = max(row, 0)
+            number_of_rows, number_of_columns = canvas.getmaxyx()
+            ship_size_rows, ship_size_columns = get_frame_size(frame)
 
-        column = min(column, ship_border_column)
-        column = max(column, 0)
+            ship_border_row = number_of_rows - ship_size_rows
+            ship_border_column = number_of_columns - ship_size_columns
+               
+                  
+            row = min(row, ship_border_row)
+            row = max(row, 0)
 
-        draw_frame(canvas, row, column, frame)
-        await asyncio.sleep(0)
+            column = min(column, ship_border_column)
+            column = max(column, 0)
 
-        draw_frame(canvas, row, column, frame, negative=True)
-        input_controls = read_controls(canvas)
-        row_direction, column_direction, is_shot_pressed = input_controls
-        row_speed, column_speed = update_speed(row_speed, column_speed, row_direction, column_direction)
+            draw_frame(canvas, row, column, frame)
+            await asyncio.sleep(0)
+            draw_frame(canvas, row, column, frame, negative=True)
 
-        row += row_speed
-        column += column_speed
+            input_controls = read_controls(canvas)
+            
+            row_direction, column_direction, is_shot_pressed = input_controls
+            row_speed, column_speed = update_speed(row_speed, column_speed, row_direction, column_direction)
 
-        for obstacle in obstacles:
-            if obstacle.has_collision(row, column, ship_size_rows, ship_size_columns):
-                coroutines.append(game_over_view)
-                return
+            row += row_speed
+            column += column_speed
 
-        seconds_left = time.time() - start_time
-        years_left = math.floor(seconds_left/1.5)
-        year = years_left + start_year
+            for obstacle in obstacles:
+                if obstacle.has_collision(row, column, ship_size_rows, ship_size_columns):
+                    coroutines.append(game_over_view)
+                    return
 
-        if year > gun_available_year and is_shot_pressed:
-            coroutines.append(fire(canvas, row, column, obstacles, obstacles_in_last_collision))
+            seconds_left = time.time() - start_time
+            years_left = math.floor(seconds_left/1.5)
+            year = years_left + start_year
+
+            if year > gun_available_year and is_shot_pressed:
+                coroutines.append(fire(canvas, row, column, obstacles, obstacles_in_last_collision))
