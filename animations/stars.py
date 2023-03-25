ef6f3bb146fba4cacfdeaec5faae99734448d781
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-25 07:57:20 +0300

    add stars animation rhytm

--- animations/stars.py
+++ animations/stars.py
@@ -1,21 +1,24 @@
 import asyncio
 import curses
 
-
 async def sleep(tics=1):
     for tic in range(tics):
         await asyncio.sleep(0)
 
 
 async def blink(canvas, row, column, symbol, offset_tics):
+    await sleep(offset_tics)
     while True:
+        
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        await sleep(offset_tics)
-
+        await sleep(20)
+       
         canvas.addstr(row, column, symbol)
-        await sleep(offset_tics)
-
+        await sleep(3)
+    
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        await sleep(offset_tics)
+        await sleep(5)
+        
         canvas.addstr(row, column, symbol)
-        await sleep(offset_tics)
+        await sleep(3)
+        
